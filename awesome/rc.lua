f0866a075af2e8869c33a1451365978e490984b6
nuke0063 <kevin@stealsyour.pw>
2014-04-20 02:00:24 -0400

    This branch is so dirty, even your mom can't clean it.

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -39,7 +39,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, and wallpapers
-beautiful.init("/usr/share/awesome/themes/default/theme.lua")
+beautiful.init("/home/nuke/.config/awesome/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "xterm"
