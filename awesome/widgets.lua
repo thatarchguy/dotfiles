59ed00808e9637f9eb437303f92980ff26fbeaa3
Kevin <kevin@stealsyour.pw>
2017-01-26 12:51:42 -0500

    Updates awesome to 4.0 api
    Signed-off-by: Kevin <kevin@stealsyour.pw>

---
+++ awesome/widgets.lua
@@ -0,0 +1,44 @@
+local wibox = require("wibox")
+
+--- User Widgets
+batterywidget = wibox.widget.textbox()
+batterywidget:set_text(" | Battery | ")
+batterywidgettimer = timer({ timeout = 5 })
+batterywidgettimer:connect_signal("timeout",
+  function()
+    fh = assert(io.popen("acpi | cut -d, -f 2,3 -", "r"))
+    batterywidget:set_text(" |" .. fh:read("*l") .. " | ")
+    fh:close()
+  end
+)
+batterywidgettimer:start()
+
+spotify_widget = wibox.widget.textbox()
+spotify_timer = timer ({timeout = 5})
+spotify_timer:connect_signal("timeout",
+  function()
+    current = io.popen("sp current-oneline", "r")
+    spotify_widget:set_text(" | " .. current:read())
+    current:close()
+  end
+)
+spotify_timer:start()
+
+
+vpnwidget = wibox.widget.textbox()
+vpnwidget:set_text(" | VPN: N/A ")
+vpnwidgettimer = timer({ timeout = 5 })
+vpnwidgettimer:connect_signal("timeout",
+  function()
+    status = io.popen("pidof openvpn", "r")
+    if status:read() == nil then
+        vpnwidget:set_markup(" | <span color='#FF0000'>VPN: OFF</span> ")
+    else
+        vpnwidget:set_markup(" | <span color='#00FF00'>VPN: ON</span> ")
+    end
+    status:close()
+  end
+)
+vpnwidgettimer:start()
+
+
